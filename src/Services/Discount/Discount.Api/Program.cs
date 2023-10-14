709b2412d8a9a0c161e8da4781decf9b67809c09
Farzad <dafarzad@gmail.com>
2023-10-14 13:03:21 +0330

    phase 15

--- src/Services/Discount/Discount.Api/Program.cs
+++ src/Services/Discount/Discount.Api/Program.cs
@@ -1,4 +1,5 @@
 using Discount.Api.Extensions;
+using Discount.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
