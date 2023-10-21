619d2e8d2cee98998b8f630a25ac4e6bc67f08a9
Farzad <dafarzad@gmail.com>
2023-10-21 10:12:52 +0330

    phase18

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailServices.cs
@@ -0,0 +1,10 @@
+﻿
+using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailServices
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
