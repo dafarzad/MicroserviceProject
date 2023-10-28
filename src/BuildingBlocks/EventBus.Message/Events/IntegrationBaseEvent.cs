fb3fb8f5b8f4ecf009637345e4e4a25f92caa13b
Farzad <dafarzad@gmail.com>
2023-10-28 11:04:29 +0330

    phase23

---
+++ src/BuildingBlocks/EventBus.Message/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace EventBus.Message.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime myProperty)
+        {
+            Id = id;
+            CreationDate = myProperty;
+        }
+
+        public Guid Id { get; }
+        public DateTime CreationDate { get; }
+    }
+}
